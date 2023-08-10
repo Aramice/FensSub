cdcf46bdce0c51125c2977b235a91765e50323cf
Aramice <141913630+Aramice@users.noreply.github.com>
2023-08-10 12:10:22 +0300

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,20 @@
+
+token = "6422610488:AAEAec8UZD3v653eImFBjt_YwHtXwOKIYLg"
+
+botname = '@FensSub_bot'
+
+admins = [@milopool,@supermarmsupport] #админы бота через запятую
+
+LITTLE_SUBCOIN_TO_GET_SUBS = 1 #минимум сабкоинов для раскрутки канала
+
+ref_count = 2 # бонус (сабкоины)реферальный
+
+REF_BONUS = 2
+
+FINE_FOR_UNSUBSCRIBING = -5 #Число должно быть отрицательным и целым
+
+SUBSCRIPTION_TERM = 1000000 #Какое время юзер должен обязательно быть подписан на канал?
+
+LINK_TO_INTRODUCTION_AND_RULES = 'https://telegra.ph/Vvedenie-v-bota-a-takzhe-pravila-06-09' #ссылка на правила и инструкцию
+
+LINK_TO_CHAT_OF_BOT = 'https://t.me/fens_sub' #ссылка на чат
